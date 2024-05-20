b2bc52c406fde38db02cb59b33f8dbfcba4dc12a
ioanasilas <ioana.silas01@e-uvt.ro>
2024-05-20 08:29:22 +0300

    updated useDaysWithoutThreats.js

--- src/hooks/useDaysWithoutThreats.js
+++ src/hooks/useDaysWithoutThreats.js
@@ -6,25 +6,21 @@ const useDaysSinceLastThreat = () => {
     const { files } = useContext(ScanHistoryContext);
 
     const calculateDaysSinceLastThreat = () => {
-      const startOfToday = new Date();
-      startOfToday.setHours(0, 0, 0, 0);
-
+        const now = new Date();
+        const startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate());  // today at 00:00
 
         const threatDates = files
             .filter(file => file.status === SCAN_STATUS.VALUES.INFECTED)
             .map(file => new Date(file.scanTime * 1000))
-            .sort((a, b) => b - a); // to get the most recent date first
+            .sort((a, b) => b - a); //get most recent date first
 
         if (threatDates.length === 0) {
             return null;
         }
 
-        const lastThreatDate = threatDates[0];
-        if (lastThreatDate >= startOfToday) {
-            return 0; // if threat was found today
-        }
+        const lastThreatDate = new Date(threatDates[0].getFullYear(), threatDates[0].getMonth(), threatDates[0].getDate()); // set to 00:00 on the day the last threat was detected
 
-        const timeDifference = startOfToday - lastThreatDate;
+        const timeDifference = startOfToday - lastThreatDate; //in miliseconds
         const daysSinceLastThreat = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
         return daysSinceLastThreat;
